9ceb2e7bfd4fd22db6b04fc87fd689925a748efb
wkflanders <wflan@seas.upenn.edu>
2025-11-05 14:33:37 -0500

    feat: some configs updates

--- .config/nvim/lua/plugins/zenbones.lua
+++ .config/nvim/lua/plugins/zenbones.lua
@@ -1,11 +1,11 @@
 return {
   "zenbones-theme/zenbones.nvim",
   name = "zenbones",
-  dependencies = "rktjmp/lush.nvim",
+  -- dependencies = "rktjmp/lush.nvim",
   lazy = true,
   -- priority = 1000,
   config = function()
-    vim.g.zenbones_compat = 1 -- Optional: for better compatibility
+    -- vim.g.zenbones_compat = 1 -- Optional: for better compatibility
     -- vim.cmd.colorscheme("rosebones")
   end,
 }
