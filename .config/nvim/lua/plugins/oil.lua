d0e0db2664301f8d9fe723a71156b80238c37ed6
wkflanders <wflan@seas.upenn.edu>
2025-12-10 01:16:22 -0500

    feat: setup oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,33 +1,33 @@
 return {
-  -- "stevearc/oil.nvim",
-  -- lazy = false,
-  -- opts = {
-  --   default_file_explorer = true,
-  --   view_options = {
-  --     show_hidden = true,
-  --   },
-  --   float = {
-  --     padding = 0,
-  --     -- max_width = 1,
-  --     -- max_height = 1,
-  --     border = "none",
-  --     win_options = {
-  --       winblend = 0,
-  --     },
-  --   },
-  --   keymaps = {
-  --     ["q"] = "actions.close",
-  --     ["<Esc>"] = "actions.close",
-  --   },
-  -- },
-  -- keys = {
-  --   -- { "<leader>E", "<CMD>Oil<CR>", desc = "Oil: inline directory" },
-  --   {
-  --     "<leader>e",
-  --     function()
-  --       require("oil").toggle_float()
-  --     end,
-  --     desc = "Oil: floating window",
-  --   },
-  -- },
+  "stevearc/oil.nvim",
+  lazy = false,
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 0,
+      -- max_width = 1,
+      -- max_height = 1,
+      border = "none",
+      win_options = {
+        winblend = 0,
+      },
+    },
+    keymaps = {
+      ["q"] = "actions.close",
+      ["<Esc>"] = "actions.close",
+    },
+  },
+  keys = {
+    -- { "<leader>E", "<CMD>Oil<CR>", desc = "Oil: inline directory" },
+    {
+      "<leader>e",
+      function()
+        require("oil").toggle_float()
+      end,
+      desc = "Oil: floating window",
+    },
+  },
 }
