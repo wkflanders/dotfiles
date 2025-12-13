4690eaa752fd2afca10c0bdc4821b08fd84df47f
wkflanders <wflan@seas.upenn.edu>
2025-12-13 10:47:27 -0500

    feat: vec and text latex snippets

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -8,5 +8,16 @@ return {
       ["<C-p>"] = { "select_prev", "fallback" },
       ["<CR>"] = false,
     })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "tex", "latex" },
+      callback = function(ev)
+        vim.keymap.set("i", "<CR>", function()
+          return require("blink.cmp").accept()
+            or vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
+        end, { buffer = ev.buf, expr = true })
+
+        vim.keymap.set("i", "<Tab>", "<Tab>", { buffer = ev.buf })
+      end,
+    })
   end,
 }
