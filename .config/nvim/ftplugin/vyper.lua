202dd8d5f7d41b9e33729eb7a462402cef0aea2f
wkflanders <wflan@seas.upenn.edu>
2026-03-04 08:23:17 -0500

    swap vyper lsp to vyper-language-server (lattica)

--- .config/nvim/ftplugin/vyper.lua
+++ .config/nvim/ftplugin/vyper.lua
@@ -1,15 +1,21 @@
 local configs = require("lspconfig.configs")
 local util = require("lspconfig.util")
 
-if not configs.vyper_lsp then
-  configs.vyper_lsp = {
+if not configs.vy_ls then
+  configs.vy_ls = {
     default_config = {
-      cmd = { vim.fn.exepath("vyper-lsp") },
+      cmd = { "vyper-language-server" },
       filetypes = { "vyper" },
       root_dir = util.root_pattern("pyproject.toml", ".git"),
       single_file_support = true,
+      on_new_config = function(new_config, root_dir)
+        local venv = root_dir .. "/.venv/bin/vyper-language-server"
+        if vim.fn.filereadable(venv) == 1 then
+          new_config.cmd = { venv }
+        end
+      end,
     },
   }
 end
 
-require("lspconfig").vyper_lsp.setup({})
+require("lspconfig").vy_ls.setup({})
